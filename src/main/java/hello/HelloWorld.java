aac2410909117e5c5d6c869195f0356dca23dc65
ewanklomp <ewanklomp@hotmail.com>
2017-01-06 10:54:09 +0100

    Initialize

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorld {
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+	
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+		SpringApplication.run(HelloWorld.class, args);
+	}
+	
+}
+
+
