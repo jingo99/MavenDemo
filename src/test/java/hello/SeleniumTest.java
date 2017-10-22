32e420a35b587d54b4ff6fdd6658f97ffceeadf6
farie82 <joepvelden@hotmail.com>
2017-10-22 13:23:18 +0200

    zou nu moeten werken met junit

--- src/test/java/hello/SeleniumTest.java
+++ src/test/java/hello/SeleniumTest.java
@@ -1,5 +1,10 @@
-package org.openqa.selenium.example;
 
+
+import junit.framework.TestCase;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.*;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,42 +12,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SeleniumTest  {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+public class SeleniumTest  extends TestCase{
+    
+    private WebDriver driver;
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+	@Before
+	public void setUp() throws Exception {
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("version", "latest");
+		capabilities.setCapability("platform", "LINUX");
+		capabilities.setCapability("name", "Testing Selenium");
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+		this.driver = new RemoteWebDriver(
+		   new URL("http://key:secret@hub.testingbot.com/wd/hub"),
+		   capabilities);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-        // Enter something to search for
-        element.sendKeys("Cheese!");
+	@Test
+	public void testSimple() throws Exception {
+		this.driver.get("http://www.google.com");
+		assertEquals("Google", this.driver.getTitle());
+	}
 
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
-
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-        
-        //Close the browser
-        driver.quit();
-    }
+	@After
+	public void tearDown() throws Exception {
+		this.driver.quit();
+	}
+    
 }
