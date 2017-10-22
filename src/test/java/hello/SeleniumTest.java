1ec1f7c8bd8d8d45677a84cb8cb39ed5ba94a163
farie82 <joepvelden@hotmail.com>
2017-10-22 15:09:24 +0200

    Update SeleniumTest.java

--- src/test/java/hello/SeleniumTest.java
+++ src/test/java/hello/SeleniumTest.java
@@ -16,7 +16,6 @@ public class SeleniumTest  extends TestCase{
 	@Before
 	public void setUp() throws Exception {
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		capabilities.browserName = "googlechrome";
 		capabilities.setPlatform(Platform.LINUX);
 		driver = new RemoteWebDriver(new URL("http://35.195.58.39:4444/wd/hub"), capabilities); 
 		
