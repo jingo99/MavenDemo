bbd00e073512960bb478f30956f7207bb4d84a97
farie82 <joepvelden@hotmail.com>
2017-10-22 14:50:42 +0200

    Update SeleniumTest.java

--- src/test/java/hello/SeleniumTest.java
+++ src/test/java/hello/SeleniumTest.java
@@ -17,7 +17,7 @@ public class SeleniumTest  extends TestCase{
 	public void setUp() throws Exception {
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 
-		driver = new RemoteWebDriver(new URL("http://35.195.58.39:4444/wd/hub"), capabilities); 
+		driver = new RemoteWebDriver(new URL("http://35.195.58.39:4445/wd/hub"), capabilities); 
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
