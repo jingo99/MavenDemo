7f7e4086d8d5717768ff5b17daa8c660c12412cd
farie82 <joepvelden@hotmail.com>
2017-10-23 10:39:43 +0200

    Update SeleniumTest.java

--- src/test/java/hello/SeleniumTest.java
+++ src/test/java/hello/SeleniumTest.java
@@ -17,6 +17,7 @@ public class SeleniumTest  extends TestCase{
 	public void setUp() throws Exception {
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setPlatform(Platform.LINUX);
+		capabilities.setCapability("marionette", true);
 		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities); 
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
